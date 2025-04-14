2f91f05abf2aca06fdf354737b47ba39ccacb607
seonoo <tjsdn22@naver.com>
2025-04-14 21:43:01 +0900

    ✨ feat: 도커 이미지 태그를 github commit id 로 변경

--- deploy/application_run.sh
+++ deploy/application_run.sh
@@ -2,11 +2,16 @@
 
 AWS_REGION=ap-northeast-2
 ECR_REGISTRY=350386634560.dkr.ecr.ap-northeast-2.amazonaws.com
-IMAGE_TAG=shinemuscat-api
 CONTAINER_NAME=shinemuscat-api
 
 # AWS ECR Login
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ECR_REGISTRY/ecr
+
+# latest image name
+IMAGE_TAG=$(aws ecr describe-images \
+  --repository-name $ECR_REPOSITORY_NAME \
+  --query 'sort_by(imageDetails,& imagePushedAt)[-1].imageTags[0]' \
+  --output text)
 
 # Pull From AWS ECR
 docker pull $ECR_REGISTRY/ecr:$IMAGE_TAG
@@ -15,7 +20,7 @@ docker pull $ECR_REGISTRY/ecr:$IMAGE_TAG
 docker images
 
 # Start Application
-docker run -d --name $CONTAINER_NAME --env PROFILE=aws -p 8080:8080 $ECR_REGISTRY/ecr:$IMAGE_TAG
+docker run -d --name $CONTAINER_NAME --env PROFILE=aws -p 80:8080 $ECR_REGISTRY/ecr:$IMAGE_TAG
 
 # Prune Images and Container
 docker image prune -af
